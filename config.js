9aeb4c9476102d15b7ca92e836b0337d79d996ab
hisham.ali <hisham.ali@fkie.fraunhofer.de>
2025-08-09 20:26:40 +0200

    add api base to config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SURVEY_CONFIG = {
   // Point to your deployed server (not GitHub Pages), e.g. Render/Fly/Railway/Heroku/VM
   // Example: 'https://your-survey-server.onrender.com/api'
-  apiBase: 'https://YOUR_SERVER_HOST/api'
+  apiBase: 'https://survey-api-26rr.onrender.com/api'
 };
